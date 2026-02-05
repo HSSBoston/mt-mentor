b89a35befee049710a2574341c42149da5178492
hssboston <hanna.s.suzuki@gmail.com>
2026-02-05 16:34:44 -0500

    Update sight-singing.py

--- sight-singing.py
+++ sight-singing.py
@@ -36,13 +36,15 @@ def midi2mp3(midiPath):
     subprocess.run(
         ["fluidsynth", "-ni", soundfont, "melody.mid", "-F", "melody.wav", "-r", "44100"],
         check=True)
-
+    subprocess.run(
+        ["ffmpeg", "-y", "-i", "melody.wav", "-codec:a", "libmp3lame", "-q:a", "4", melody.mp3],
+        check=True)
 score = generateSightSingingScore()
 score.write("musicxml", "melody.xml")
 mxml2img("melody.xml")
 mxml2midi("melody.xml")
 midi2mp3("melody.mid")
 
-st.title("Section 2B: Signt-singing")
+st.title("Section 2B: Sight-singing")
 st.image( Image.open("melody-image-1.png") )
-st.audio("melody.wav")
+st.audio("melody.mp3")
