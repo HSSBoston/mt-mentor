b1685262852a3d093b905caaccd1370268ff6900
hssboston <hanna.s.suzuki@gmail.com>
2026-02-05 16:00:07 -0500

    Update sight-singing.py

--- sight-singing.py
+++ sight-singing.py
@@ -4,28 +4,28 @@ from PIL import Image
 import os, subprocess, shutil
 from sight_singing_gen import *
 
-def findMusescoreCmd():
+def findMuseScorePath():
     for cmd in ("musescore", "mscore", "musescore3"):
         if shutil.which(cmd):
             return cmd
     raise RuntimeError("MuseScore executable not found on PATH.")
 
-MUSESCORE_PATH = findMusescoreCmd()
-# st.write(MUSESCORE_CMD)
+MUSESCORE_PATH = findMuseScorePath()
+# st.write(MUSESCORE_PATH)
 
 if not shutil.which("xvfb-run"):
     raise RuntimeError("xvfb-run not found on PATH.")
 
-def score2imgMidi(score):
+def mxml2imgMidi(mxmlPath):
     env = os.environ.copy()
     env["QT_QPA_PLATFORM"] = "offscreen"
 
-#     subprocess.run(
-#         ["xvfb-run", "-a", MUSESCORE_APPIMAGE, "-o", "melody-image.png", mxmlPath],
-#         check=True,
-#         env=env)
+    subprocess.run(
+        ["xvfb-run", "-a", MUSESCORE_PATH, "-o", "melody-image.png", mxmlPath],
+        check=True,
+        env=env)
     
-    score.write("musicxml.png", fp = "melody-image.png")
+#     score.write("musicxml.png", fp = "melody-image.png")
 #     score.write("midi",         fp = "melody.mid")
     
 
@@ -34,9 +34,10 @@ def score2imgMidi(score):
 #     subprocess.run(cmd, check=True)
 
 score = generateSightSingingScore()
-score2imgMidi(score)
+score.write("musicxml", "melody.xml")
+mxml2imgMidi("melody.xml")
 # score2mp3(score)
 
 st.title("Sight singing question")
-st.image( Image.open("melody-image-1.png") )
+st.image( Image.open("melody-image.png") )
 # st.audio("melody.mp3")
